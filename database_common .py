df965f390cc78c943bdf945eb3fdc5599ec07e15
Hanga Gelencser <hangagelencser@gmail.com>
2017-10-15 10:38:25 +0200

    added database_common

--- database_common .py
+++ database_common .py
@@ -1,10 +1,24 @@
 # Creates a decorator to handle the database connection/cursor opening/closing.
 # Creates the cursor with RealDictCursor, thus it returns real dictionaries, where the column names are the keys.
-from config_file import Config
+#from config import Config
+
+import os
 import psycopg2
 import psycopg2.extras
+import urllib
 
-
+'''
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+'''
+'''
 def open_database():
     try:
         connection_string = Config.DB_CONNECTION_STR
@@ -13,16 +27,25 @@ def open_database():
     except psycopg2.DatabaseError as exception:
         print(exception)
     return connection
+'''
 
 
 def connection_handler(function):
     def wrapper(*args, **kwargs):
-        connection = open_database()
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+        connection = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
+        connection.autocommit = True
         dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         ret_value = function(dict_cur, *args, **kwargs)
         dict_cur.close()
         connection.close()
         return ret_value
     return wrapper
-
