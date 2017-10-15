45ec085141f13050b6078e970b8be7ff74ce06a0
Hanga Gelencser <hangagelencser@gmail.com>
2017-10-15 10:44:37 +0200

    database common

--- config_file.py
+++
@@ -1,7 +0,0 @@
-# It is listed in the .gitignore file so it is not version controlled
-class Config:
-    DB_NAME = "hanga"
-    DB_USER = "hanga"
-    DB_HOST = "localhost"
-    DB_PWD = "yupiyay"
-    DB_CONNECTION_STR = "dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PWD + "'"
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import user_handler
 import database_common
 
 app = Flask(__name__)
+app.secret_key = "jujujuj"
 
 
 @app.route("/")
@@ -55,9 +56,3 @@ def logout():
     return redirect('/')
 
 
-if __name__ == "__main__":
-    app.secret_key = "jujujuj"
-    app.run(
-        debug=True,
-        port=5000,
-    )
